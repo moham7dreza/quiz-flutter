fb91371922f6cef1a4ba3f782fa4dc466ec7d3e2
Mohamadreza Rezaei <clash.trial@gmail.com>
2023-01-12 21:01:40 +0330

    work with some useful widgets

--- lib/example/telegram.dart
+++ lib/example/telegram.dart
@@ -7,7 +7,10 @@ class Telegram extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Telegram'),
+        title: const Text(
+          'Telegram',
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.amber,
         centerTitle: true,
         actions: <Widget>[
@@ -24,26 +27,137 @@ class Telegram extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-            elevation: 20,
-            backgroundColor: Colors.amber,
-          ),
-          child: const Text(
-            'click',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/images/image.png'),
             ),
           ),
-        ),
-      ),
+          const Card(
+            margin: EdgeInsets.all(20),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: Colors.greenAccent,
+              ),
+              title: Text(
+                '03366655477',
+                style: TextStyle(
+                  fontFamily: 'googleSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 200.0,
+            height: 20.0,
+            child: Divider(color: Colors.amber),
+          ),
+          Center(
+              child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: TextField(
+              controller: TextEditingController(),
+              decoration: const InputDecoration(
+                labelText: 'UserName',
+                hintText: 'UserName',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+              maxLength: 191,
+              obscureText: true,
+              cursorColor: Colors.amber,
+              keyboardType: TextInputType.phone,
+            ),
+          )),
+          // IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          const Spacer(),
+          ElevatedButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              // primary: Colors.black,
+              elevation: 20,
+              backgroundColor: Colors.amber,
+              // textStyle: const TextStyle(color: Colors.red),
+              minimumSize: const Size(400, 60),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+            ),
+            child: const Text(
+              'click',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          // TextButton(
+          //   onPressed: () {},
+          //   child: const Text(
+          //     'CODEX',
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //       fontFamily: 'googleSans',
+          //       fontWeight: FontWeight.bold,
+          //     ),
+          //   ),
+          // ),
+          // TextButton.icon(
+          //   onPressed: () {},
+          //   icon: const Icon(Icons.home),
+          //   label: const Text(
+          //     'CODEX',
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //       fontFamily: 'googleSans',
+          //       fontWeight: FontWeight.bold,
+          //     ),
+          //   ),
+          // ),
+          // MaterialButton(
+          //   onPressed: () {},
+          //   shape: RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.circular(25.0),
+          //   ),
+          //   color: Colors.amber,
+          //   splashColor: Colors.black,
+          //   child: const Text(
+          //     'CODEX',
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //       fontFamily: 'googleSans',
+          //       fontWeight: FontWeight.bold,
+          //     ),
+          //   ),
+          // ),
+          Container(
+            margin: const EdgeInsets.only(left: 400, bottom: 25),
+            child: FloatingActionButton(
+              onPressed: () {},
+              tooltip: 'Increment',
+              elevation: 70,
+              splashColor: Colors.amber,
+              backgroundColor: Colors.amber,
+              child: const Icon(
+                Icons.contact_phone,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      )),
       drawer: Drawer(
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
-              accountName: Text('codex'),
+              accountName: const Text('codex'),
               accountEmail: const Text(
                 'clash@gmail.com',
                 style: TextStyle(fontSize: 20.0),
@@ -60,25 +174,30 @@ class Telegram extends StatelessWidget {
               decoration: const BoxDecoration(
                 // color: Colors.amber,
                 gradient: LinearGradient(colors: [Colors.pink, Colors.purple]),
+                // image: DecorationImage(
+                //   image: NetworkImage(
+                //       'https://docs.flutter.dev/assets/images/flutter-logo-sharing.png'),
+                //       fit: BoxFit.cover,
+                // ),
               ),
             ),
             const ListTile(
               title: Text('settings'),
               leading: Icon(Icons.settings),
             ),
-            ListTile(
+            const ListTile(
               title: Text('settings'),
               leading: Icon(Icons.settings),
             ),
-            ListTile(
+            const ListTile(
               title: Text('settings'),
               leading: Icon(Icons.settings),
             ),
-            ListTile(
+            const ListTile(
               title: Text('settings'),
               leading: Icon(Icons.settings),
             ),
-            ListTile(
+            const ListTile(
               title: Text('settings'),
               leading: Icon(Icons.settings),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Techzilla Store',
+      title: 'my app',
       theme: ThemeData(
         // This is the theme of your application.
         //
